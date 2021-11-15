02b0aec2f8278818b1b886419e490239c7e4836a
Mark Andrew Miller <MAM@lbl.gov>
2021-11-15 13:29:11 -0500

    clean up
    PyCharm does not like 'import badexperiment.sheet2yaml as s2y' in becli.py

--- badexperiment/badexperiment.py
+++
@@ -1,9 +0,0 @@
-# class badexp(userinput):
-# 	"""docstring for ClassName"""
-# 	def __init__(self, arg):
-# 		super(ClassName, self).__init__()
-# 		self.arg = arg
-# 	print("hello")
-
-def hello():
-    return "hello"
